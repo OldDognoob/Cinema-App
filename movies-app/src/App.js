ce6a9c9228fab5f4181e0eb8668fa2d095ba03d1
Dimos-Christidis <christidis.dimosthenis@gmail.com>
2020-06-30 14:20:56 +0300

    create new files

--- movies-app/src/App.js
+++ movies-app/src/App.js
@@ -1,15 +1,37 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Search from './components/Search'
 
 
 function App() {
+  // s is our search query when we type and search our movie
+  const [state, setState] = useState({
+    s: "",
+    results:[],
+    selected:{}
+  });
+  const apiurl = "http://www.omdbapi.com/?apikey=dfe6d885";
+
+  // handle our value when we are typing
+  const handleInput = (event) =>{
+    let s = event.target.value;
+    setState(previousState => {
+      return {...previousState, s: s}
+
+      console.log(state.s)
+    })
+
+
+  }
+
+
+
   return (
     <div className="App">
       <header className="App-header">
       <h1>Movie Database</h1>
       </header>
       <main>
-        <Search/>
+        <Search handleInput={handleInput}/>
       </main>
   
     </div>
